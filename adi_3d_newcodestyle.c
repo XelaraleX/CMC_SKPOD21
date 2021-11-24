63a1739b6960f77affc78647cf3048321edb05a7
Alexander Romanov <74380662+XelaraleX@users.noreply.github.com>
2021-11-24 17:51:28 +0300

    Update adi_3d_newcodestyle.c

--- adi_3d_newcodestyle.c
+++ adi_3d_newcodestyle.c
@@ -1,6 +1,8 @@
 #include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
+#include <sys/time.h>
 
 #define  Max(a,b) ((a)>(b)?(a):(b))
 #define  N   (2 * 2 * 2 * 2 * 2 * 2 + 2)
@@ -14,21 +16,42 @@ double A [N][N][N];
 
 void relax();
 void init();
-void verify(); 
+void verify();
+double rtclock();
+
+double 
+rtclock()
+{
+    struct timeval Tp;
+    int stat;
+    stat = gettimeofday (&Tp, NULL);
+    if (stat != 0) {
+    	printf ("Error return from gettimeofday: %d", stat);
+	}
+    return (Tp.tv_sec + Tp.tv_usec * 1.0e-6);
+}
 
 int 
 main(int an, char **as)
 {
 	int it;
 	init();
+	double timer = 0.;
+	int cnt = 0;
 	for (it = 1; it <= itmax; ++it) {
+		cnt++;
 		eps = 0.;
+		double bench_t_start = rtclock();
 		relax();
+		double bench_t_end = rtclock();
+  		printf ("Time in seconds = %0.6lf\n", bench_t_end - bench_t_start);
+		timer += bench_t_end - bench_t_start;
 		printf( "it=%4i   eps=%f\n", it, eps);
 		if (eps < maxeps) {
 			break;
 		}
 	}
+	printf ("Average time= %0.6lf\n", timer / cnt);
 	verify();
 	return 0;
 }
@@ -60,10 +83,10 @@ relax()
 			}
 		}
 	}
-	for (int k = 1; k <= N - 2; ++k) {
-		for (int j = 1; j <= N - 2; ++j) {
-			for (int i = 1; i <= N - 2; ++i) {
-				A[i][j][k] =(A[i][j - 1][k] + A[i][j + 1][k]) / 2.;
+	for (k = 1; k <= N - 2; ++k) {
+		for (j = 1; j <= N - 2; ++j) {
+			for (i = 1; i <= N - 2; ++i) {
+				A[i][j][k] = (A[i][j - 1][k] + A[i][j + 1][k]) / 2.;
 			}
 		}
 	}
