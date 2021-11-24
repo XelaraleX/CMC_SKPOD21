4c7bf940ac6dd744b0fc41ad683aa6eaf3228bf9
Alexander Romanov <74380662+XelaraleX@users.noreply.github.com>
2021-11-24 20:58:24 +0300

    Update adi_3d_v1_time.c

--- adi_3d_v1_time.c
+++ adi_3d_v1_time.c
@@ -3,13 +3,12 @@
 #include <stdio.h>
 #include <time.h>
 #include <sys/time.h>
-
 #define  Max(a,b) ((a)>(b)?(a):(b))
-#define  N   (2 * 2 * 2 * 2 * 2 * 2 + 2)
 
+#define  N   (2*2*2*2*2*2+2)
 double   maxeps = 0.1e-7;
 int itmax = 100;
-int i, j, k;
+int i,j,k;
 
 double eps;
 double A [N][N][N];
@@ -19,7 +18,7 @@ void init();
 void verify();
 double rtclock();
 
-double 
+double
 rtclock()
 {
     struct timeval Tp;
@@ -31,83 +30,79 @@ rtclock()
     return (Tp.tv_sec + Tp.tv_usec * 1.0e-6);
 }
 
-int 
-main(int an, char **as)
+int main(int an, char **as)
 {
 	int it;
-	double bench_t_start = rtclock();
+    double bench_t_start = rtclock();
 	init();
-	for (it = 1; it <= itmax; ++it) {
+
+	for(it=1; it<=itmax; it++)
+	{
 		eps = 0.;
 		relax();
-		//printf( "it=%4i   eps=%f\n", it, eps);
-		if (eps < maxeps) {
-			break;
-		}
+		//printf( "it=%4i   eps=%f\n", it,eps);
+		if (eps < maxeps) break;
 	}
+
 	verify();
-	double bench_t_end = rtclock();
-	printf ("Time in seconds = %0.6lf\n", bench_t_end - bench_t_start);
+    double bench_t_end = rtclock();
+    printf ("Time in seconds = %0.6lf\n", bench_t_end - bench_t_start);
+
 	return 0;
 }
 
 
-void 
-init()
+void init()
 { 
-	for (k = 0; k <= N - 1; ++k) {
-		for (j = 0; j <= N - 1; ++j) {
-			for (i = 0; i <= N - 1; ++i) {
-				if (i == 0 || i == N - 1 || j == 0 || j == N - 1 || k == 0 || k == N - 1) {
-					A[i][j][k] = 0.;
-				} else {
-					A[i][j][k] = ( 4. + i + j + k);
-				}
-			}
-		}
+	for(k=0; k<=N-1; k++)
+	for(j=0; j<=N-1; j++)
+	for(i=0; i<=N-1; i++)
+	{
+		if(i==0 || i==N-1 || j==0 || j==N-1 || k==0 || k==N-1)
+		A[i][j][k]= 0.;
+		else A[i][j][k]= ( 4. + i + j + k) ;
 	}
 } 
 
-void 
-relax()
+void relax()
 {
-	for (k = 1; k <= N - 2; ++k) {
-		for (j = 1; j <= N - 2; ++j) {
-			for (i = 1; i <= N - 2; ++i) {
-				A[i][j][k] = (A[i - 1][j][k] + A[i + 1][j][k]) / 2.;
-			}
-		}
+	for(k=1; k<=N-2; k++)
+	for(j=1; j<=N-2; j++)
+	for(i=1; i<=N-2; i++)
+	{
+		A[i][j][k] = (A[i-1][j][k]+A[i+1][j][k])/2.;
 	}
-	for (k = 1; k <= N - 2; ++k) {
-		for (j = 1; j <= N - 2; ++j) {
-			for (i = 1; i <= N - 2; ++i) {
-				A[i][j][k] = (A[i][j - 1][k] + A[i][j + 1][k]) / 2.;
-			}
-		}
+
+	for(k=1; k<=N-2; k++)
+	for(j=1; j<=N-2; j++)
+	for(i=1; i<=N-2; i++)
+	{
+		A[i][j][k] =(A[i][j-1][k]+A[i][j+1][k])/2.;
 	}
-	for (k = 1; k <= N - 2; ++k) {
-		for (j = 1; j <= N - 2; ++j) {
-			for (i = 1; i <= N - 2; ++i) {
-				double e;
-				e = A[i][j][k];
-				A[i][j][k] = (A[i][j][k - 1] + A[i][j][k + 1]) / 2.;
-				eps = Max(eps,fabs(e - A[i][j][k]));
-			}
-		}
+
+	for(k=1; k<=N-2; k++)
+	for(j=1; j<=N-2; j++)
+	for(i=1; i<=N-2; i++)
+	{
+		double e;
+		e=A[i][j][k];
+		A[i][j][k] = (A[i][j][k-1]+A[i][j][k+1])/2.;
+		eps=Max(eps,fabs(e-A[i][j][k]));
 	}
+
 }
 
-void 
-verify()
+void verify()
 {
 	double s;
-	s = 0.;
-	for (k = 0; k <= N - 1; ++k) {
-		for (j = 0; j <= N - 1; ++j) {
-			for (i = 0; i <= N - 1; ++i) {
-				s = s + A[i][j][k] * (i + 1) * (j + 1) * (k + 1) / (N * N * N);
-			}
-		}
+
+	s=0.;
+	for(k=0; k<=N-1; k++)
+	for(j=0; j<=N-1; j++)
+	for(i=0; i<=N-1; i++)
+	{
+		s=s+A[i][j][k]*(i+1)*(j+1)*(k+1)/(N*N*N);
 	}
 	//printf("  S = %f\n",s);
+
 }
